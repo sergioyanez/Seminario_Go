77329bca7840f98acf38b679ec55d4a3bb8f5552
sergioyanez <sergiomyanez02@gmail.com>
2021-09-15 02:32:28 -0300

    Se agregan comentarios

--- clase2.2/main.go
+++ clase2.2/main.go
@@ -12,13 +12,13 @@ func main() {
 		fmt.Printf("%d:%q", i, c)
 	}
 	fmt.Println()
-	fmt.Println(country[1:3])
-	fmt.Println(country[:4])
-	fmt.Println(country[2:])
+	fmt.Println(country[1:3]) // retorna la variable country desde el indice 1 al 3-1 ( retorna ap)
+	fmt.Println(country[:4])  // retorna la variable country desde el indice 0 al 4-1 ( retorna Japa)
+	fmt.Println(country[2:])  // retorna la variable country desde el indice 2 al final ( retorna pan)
 
-	fmt.Println(strings.Contains("Japan", "a"))
+	fmt.Println(strings.Contains("Japan", "a")) // retorna si la variable country contiene el string a (retorna true)
 
-	fmt.Println(strings.Count("Japan", "a"))
+	fmt.Println(strings.Count("Japan", "a")) // retorna cuántas veces tien la variable country el string a (retorna 2)
 
-	fmt.Println("Japan" < "japan")
+	fmt.Println("Japan" < "japan") //retorna si el string Japan es menor que japan ( retorna true)
 }
