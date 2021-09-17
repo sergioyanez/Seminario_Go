af2ad905718c4a73a811c0a5e833bc84547cfc33
sergioyanez <sergiomyanez02@gmail.com>
2021-09-17 18:56:56 -0300

    Se realizo un nuevo testeo

--- tpEntregable/main_test.go
+++ tpEntregable/main_test.go
@@ -1,7 +1,12 @@
 package main //porque estoy en el mismo nivel
 
 import "testing"
+import "entregableGoTudai2021/model"
 
 func TestMain(t *testing.T) {
 	main()
+}
+
+func TestNewCadenaEntrante(t *testing.T) {
+	model.NewCadenaEntrante("TX03ABC")
 }
