c44f03cc0e67bd8e4452de9b0e70c017e968bee0
sergioyanez <sergiomyanez02@gmail.com>
2021-09-18 19:08:18 -0300

    Se modifican testeos

--- tpEntregable/entities_test.go
+++ tpEntregable/entities_test.go
@@ -13,3 +13,29 @@ func TestChequeoDeCadena(t *testing.T) {
 	largo := model.GetLength(cadena.Value)
 	assert.Equal(t, model.ChequeoDeCadena(tipo, valor, largo), true, "La cadena es invalida")
 }
+
+func TestParser(t *testing.T) {
+	var cases = []struct {
+		Input   string // input string in order to be parsed
+		Success bool   // paser result
+		Type    string // the input type
+		Value   string // the input value
+		Length  int    // value length
+	}{
+		{"TX02AB", true, "TX", "AB", 2},
+		{"NN100987654321", true, "NN", "0987654321", 10},
+		{"TX06ABCDE", false, "", "", 0},
+		{"NN04000A", false, "", "", 0},
+		//	{"TX3", false, "", "", 3},
+	}
+
+	for _, testData := range cases {
+		data, err := generarResultado(testData.Input)
+		error2 := (err == nil)
+		assert.Equal(t, data.Type, testData.Type)
+		assert.Equal(t, data.Value, testData.Value)
+		assert.Equal(t, data.Length, testData.Length)
+		assert.Equal(t, error2, testData.Success)
+
+	}
+}
--- tpEntregable/main.go
+++ tpEntregable/main.go
@@ -7,26 +7,28 @@ import (
 )
 
 func generarResultado(cad string) (model.Result, error) {
-	cadena := model.NewCadenaEntrante(cad)
-	tipo := model.GetTipo(cadena.Value)
-	valor := model.GetValue(cadena.Value)
-	largo := model.GetLength(cadena.Value)
+	if len(cad) > 4 {
+		cadena := model.NewCadenaEntrante(cad)
+		tipo := model.GetTipo(cadena.Value)
+		valor := model.GetValue(cadena.Value)
+		largo := model.GetLength(cadena.Value)
+		if model.ChequeoDeCadena(tipo, valor, largo) {
+			return model.NewResult(tipo, largo, valor), nil
 
-	if model.ChequeoDeCadena(tipo, valor, largo) {
-
-		return model.NewResult(tipo, largo, valor), nil
-
+		}
 	}
 	return model.Result{}, errors.New("cadena Invalida")
 
 }
 
 func main() {
-	cadena := model.NewCadenaEntrante("NN03487")
+	nuevaCadena := "NN03897"
 
-	fmt.Println(model.GetTipo(cadena.Value))
-	fmt.Println(model.GetValue(cadena.Value))
-	fmt.Println(model.GetLength(cadena.Value))
-	fmt.Println(generarResultado("NN03487"))
+	result, err := generarResultado(nuevaCadena)
+	if err == nil {
+		fmt.Println(result)
+	} else {
+		fmt.Println(err)
+	}
 
 }
